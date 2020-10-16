ed96dcdc441106839654e3264ac3d9e3b27ef163
Carlos Barcos <fbarcos8@hotmail.com>
2020-10-16 14:37:20 -0500

    Se agrega basico de llamada a api y hooks para la respectiva llamada

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { useState } from 'react';
 import { Container,Row,Col,FormControl, InputGroup,Button, Jumbotron } from 'react-bootstrap';
+import {CONFIG} from './config';
 
 export const App = () => {
+  const [city, setCity] = useState("Guayaquil")
+  const handleClick = ()=>{
+    console.log(city)
+    getClima(city)
+  }
+  const handleChange = ()=>{
+    console.log(city)
+  }
+  const getClima = (city)=>{
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+CONFIG.api_key
+    console.log(url);
+  }
+
   return (
     <Container className="p-5">
       <Row>
@@ -10,11 +24,11 @@ export const App = () => {
             <InputGroup.Prepend>
               <InputGroup.Text id="inputGroup-sizing-sm">Ciudad</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+            <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" value={city} onChange={handleChange}/>
           </InputGroup>
         </Col>
         <Col xs={1}>
-          <Button variant="primary">Clima</Button>
+          <Button variant="primary" onClick={handleClick}>Clima</Button>
         </Col>
       </Row>
       <Row>
