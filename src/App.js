24b68a8b83c2082ee7d3650401b3a5abf7706bea
Carlos Barcos <fbarcos8@hotmail.com>
2020-10-16 13:57:09 -0500

    Se agrega boostrap 	nuevo archivo:  package-lock.json 	modificado:     package.json 	modificado:     src/App.js 	modificado:     src/index.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Container,Row,Col,FormControl, InputGroup,Button, Jumbotron } from 'react-bootstrap';
 
 export const App = () => {
   return (
-    <>
-     Welcome to Weather App
-    </>
+    <Container className="p-5">
+      <Row>
+        <Col xs={11}>
+          <InputGroup size="sm" className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text id="inputGroup-sizing-sm">Ciudad</InputGroup.Text>
+            </InputGroup.Prepend>
+            <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+          </InputGroup>
+        </Col>
+        <Col xs={1}>
+          <Button variant="primary">Clima</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12}>
+        <Jumbotron>
+          
+        </Jumbotron>
+        </Col>
+      </Row>
+
+    </Container>
   )
 }
