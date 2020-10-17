a552c41a8cfc68521f6830bc2ef6cb3df5d6b8a9
carlosbarcos <carlosbarcos@outlook.com>
2020-10-17 03:09:26 -0500

    Se realiza cambios para obtener el icono del asociado al clima y se agrega estilos.
    	modificado:     src/components/Jumboweather.js
    	modificado:     src/helpers/getWeather.js
    	modificado:     src/index.js
    	nuevo archivo:  src/styles.css

--- src/helpers/getWeather.js
+++ src/helpers/getWeather.js
@@ -4,13 +4,10 @@ export const getWeather = async (city)=>{
   const url = `${CONFIG.url_base}/weather?appid=${CONFIG.api_key}&lang=${CONFIG.lang}&units=${CONFIG.units}&q=${encodeURI(city)}`;
   const res = await fetch(url);
   const data = await res.json();
-  /*console.log(data.weather[0].description);
-  console.log(data.main.temp);
-  console.log(data.main.temp_min,data.main.temp_max);
-  console.log(data.main.pressure);
-  console.log(data.main.humidity);*/
+
   return {
     weather: data.weather[0].description,
+    weather_icon : data.weather[0].icon,
     temp : data.main.temp,
     temp_min : data.main.temp_min,
     temp_max : data.main.temp_max,
