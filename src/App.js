849afcb24d5cbf3ed93b8839ec9640761f13bd51
Nikhil BN <nikhilbnaradhya1.gem@gmail.com>
2020-10-17 12:48:23 +0530

    Handled invalid city error

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
 import React, { useState } from 'react';
-import { Container,Row,Col,FormControl, InputGroup,Button, Jumbotron } from 'react-bootstrap';
+import { Container,Row,Col,FormControl, InputGroup,Button, Jumbotron, Spinner } from 'react-bootstrap';
 //import {CONFIG} from './config';
-import {getWeather} from './helpers/getWeather';
 import { useFetchWeather } from './hooks/useFetchWeather';
 
 export const App = () => {
 
-  const [city, setCity] = useState("Guayaquil")
+  const [city, setCity] = useState("Guayaquil");
+  const [finalCity, setFinalCity] = useState("Guayaquil");
 
-  const {data , loading} = useFetchWeather(city);
+  const {data , loading, isError} = useFetchWeather(finalCity);
 
   const handleClick = (e)=>{
-    console.log(city)
-    getWeather(city).then((data)=>{
-      console.log(data);
-    });
+    e.preventDefault();
+    setFinalCity(city);
   }
 
   const handleChange = (e)=>{
     setCity(e.target.value)
   }
-  
+
+  const style = { position: "fixed", top: "30%", left: "50%" };
+  let output;
+  if(loading) {
+    output = <Spinner style={style} animation="border" />
+  } else {
+    if(isError) {
+      output = <div style={style}>Invalid City</div>;
+    } else {
+      output = <Row>
+                <Col xs={12}>
+                <Jumbotron>
+                  <p><strong>Clima:</strong> {data.weather}</p>
+                  <p><strong>Temperatura:</strong> {data.temp}</p>
+                  <p><strong>Humedad:</strong> {data.humidity}</p>
+                </Jumbotron>
+                </Col>
+              </Row>;
+    }
+  }
 
   return (
     <Container className="p-5">
@@ -30,23 +47,14 @@ export const App = () => {
             <InputGroup.Prepend>
               <InputGroup.Text id="inputGroup-sizing-sm">Ciudad</InputGroup.Text>
             </InputGroup.Prepend>
-            <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" value={city} onChange={handleChange}/>
+            <FormControl aria-label="Small" aria-describedby="inputGroup-sizing-sm" value={city} onChange={handleChange} />
           </InputGroup>
         </Col>
         <Col xs={1}>
           <Button variant="primary" onClick={handleClick}>Clima</Button>
         </Col>
       </Row>
-      <Row>
-        <Col xs={12}>
-        <Jumbotron>
-          <p><strong>Clima:</strong> {data.weather}</p>
-          <p><strong>Temperatura:</strong> {data.temp}</p>
-          <p><strong>Humedad:</strong> {data.humidity}</p>
-        </Jumbotron>
-        </Col>
-      </Row>
-
+      {output}
     </Container>
   )
 }
